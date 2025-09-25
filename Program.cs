47983c816db6dd0f39b25da26e933958fe3a8eb9
Andrej Trajkovic <700007609@GoTVDSB.ca>
2025-09-25 14:28:19 -0400

    done

--- Program.cs
+++ Program.cs
@@ -7,74 +7,121 @@
 
             // #1 - Counting  with Conditional Loops
 
-            //int i = 10;
-            //while (i >= 1)
-            //{
-            //    Console.WriteLine(i);
-            //    i -= 2;
-            //}
-            //Console.WriteLine();
+            int i = 10;
+            while (i >= 1)
+            {
+                Console.WriteLine(i);
+                i -= 2;
+            }
+            Console.WriteLine();
 
             // #2 - Vaild Number
 
-            //int number;
-            //double number1;
-            //Console.WriteLine("Please enter an integer: ");
-            //while (!Int32.TryParse(Console.ReadLine(), out number))
-            //while(!Double.TryParse(Console.ReadLine(), out number1))
-            //{
-            //    Console.WriteLine("Invalid integer, try again.");
-            //}
-            //Console.WriteLine("Thanks for " + number + "!");
+            int number;
+            double number1;
+            Console.WriteLine("Please enter an integer: ");
+            while (!Int32.TryParse(Console.ReadLine(), out number))
+                while (!Double.TryParse(Console.ReadLine(), out number1))
+                {
+                    Console.WriteLine("Invalid integer, try again.");
+                }
+            Console.WriteLine("Thanks for " + number + "!");
 
             // #3 - End at Any unknown Time
-            //Random generator = new Random();
-            //int guesses = 0, guess, secrectNum;
-            //secrectNum = generator.Next(1, 11);
+            Random generator = new Random();
+            int guesses = 0, guess, secrectNum;
+            secrectNum = generator.Next(1, 11);
 
-            //bool done = false;
-            //while (!done)
-            //{
-            //    Console.WriteLine("Guess a number from 1-10, or -1 to quit.");
-            //    while (!Int32.TryParse(Console.ReadLine(), out guess))
-            //    {
-            //        Console.WriteLine("Invalid integer, try again.");
-            //        guesses++;
-            //    }
+            bool done = false;
+            while (!done)
+            {
+                Console.WriteLine("Guess a number from 1-10, or -1 to quit.");
+                while (!Int32.TryParse(Console.ReadLine(), out guess))
+                {
+                    Console.WriteLine("Invalid integer, try again.");
+                    guesses++;
+                }
 
-            //    guesses++;
-            //    if(guess == secrectNum)
-            //    {
-            //        done = true;
-            //        Console.WriteLine("You Win");
-            //    }
-            //    else if ( guess == -1)
-            //    {
-            //           done = true;
-            //        Console.WriteLine("User quits, thanks for playing!");
-            //    }
-            //    else if (guess >= 11)
-            //    {
-            //        done = true;
-            //        Console.WriteLine("Out of guesses, you lose. Thanks for playing");
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("Incorrect, guess again");
-            //    }
-            //}
+                guesses++;
+                if (guess == secrectNum)
+                {
+                    done = true;
+                    Console.WriteLine("You Win");
+                }
+                else if (guess == -1)
+                {
+                    done = true;
+                    Console.WriteLine("User quits, thanks for playing!");
+                }
+                else if (guess >= 11)
+                {
+                    done = true;
+                    Console.WriteLine("Out of guesses, you lose. Thanks for playing");
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect, guess again");
+                }
+            }
 
             // #4 - Verifying Input Ranges
 
             double bet, wallet = 100;
             Console.WriteLine("How much money do you want to bet? You have $" + wallet);
             bet = Convert.ToInt32(Console.ReadLine());
-            while (bet > wallet || bet > 0)
+            while (bet > wallet || bet < 0)
             {
                 Console.WriteLine("That is more than you have, please enter a vlaid amount: ");
                 bet = Convert.ToInt32(Console.ReadLine());
             }
             Console.WriteLine("Thanks for betting a reasonable amount");
+
+            // #5 - Do...While
+
+            string selection;
+            do
+            {
+                Console.WriteLine("Who is the better captain?");
+                Console.WriteLine("a - Captain Picard");
+                Console.WriteLine("b - Captain  Kirk");
+                selection = Console.ReadLine();
+            }
+            while (selection != "a");
+            {
+                Console.WriteLine("Correct!");
+            }
+
+            // Practice Problems
+
+            // #1 
+
+            int choices;
+            Console.WriteLine("Who is the best ninja turtle?");
+            do
+            {
+                Console.WriteLine("1 - Leonardo");
+                Console.WriteLine("2 - Donatello");
+                Console.WriteLine("3 - Raphael");
+                Console.WriteLine("4 - Michelangelo");
+                while (!Int32.TryParse(Console.ReadLine(), out choices))
+                    Console.WriteLine("Invaild intput, enter an integer: ");
+                if (choices != 4)
+                {
+                    Console.WriteLine("Try again.");
+                }
+            }
+            while (choices != 4);
+            Console.WriteLine("Good chioce my freind.");
+
+            // #2 
+
+            int age;
+
+            Console.WriteLine("Please enter in your age.");
+            while (!Int32.TryParse(Console.ReadLine(), out age) || age <= 0)
+                Console.WriteLine("Invaild input, try again: ");
+            Console.WriteLine("Thanks");
+         
         }
     }
 }
